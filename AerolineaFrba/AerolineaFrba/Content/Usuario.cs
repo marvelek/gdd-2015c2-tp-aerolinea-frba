b11664751450ae03d18dc34f0743dc60cea62032
tomasyagas <tomas.yagas@gmail.com>
2015-10-18 12:12:03 -0300

    configuro y agrego dataset. Empiezo con login de usuario

---
+++ AerolineaFrba/AerolineaFrba/Content/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AerolineaFrba.GD2C2015DataSetTableAdapters;
+
+namespace AerolineaFrba.Content
+{
+    class Usuario
+    {
+
+        private UsuariosTableAdapter usuariosTableAdapter = new UsuariosTableAdapter();
+        private GD2C2015DataSet dataSet = new GD2C2015DataSet();
+        private GD2C2015DataSet.UsuariosRow usuarioRow;
+
+
+
+
+
+        public virtual DateTime getFecha()
+        {
+            return AerolineaFrba.Properties.Settings.Default.FechaSistema;
+        }
+    }
+}
