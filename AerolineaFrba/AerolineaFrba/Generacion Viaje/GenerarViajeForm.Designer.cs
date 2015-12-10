7c7cef9bae9d42c4b21f2766bc770f5414ac3ac9
tomasyagas <tomas.yagas@gmail.com>
2015-12-10 00:36:10 -0300

    saco los comboBox de generar viaje y los dejo como corresponde

--- AerolineaFrba/AerolineaFrba/Generacion Viaje/GenerarViajeForm.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Generacion Viaje/GenerarViajeForm.Designer.cs
@@ -30,22 +30,24 @@
         {
             this.generar = new System.Windows.Forms.Button();
             this.limpiar = new System.Windows.Forms.Button();
-            this.rutas = new System.Windows.Forms.ComboBox();
             this.fechaSalida = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.fechaLlegadaEstimada = new System.Windows.Forms.DateTimePicker();
+            this.destino = new System.Windows.Forms.TextBox();
+            this.origen = new System.Windows.Forms.TextBox();
+            this.matricula = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.aeronaves = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // generar
             // 
-            this.generar.Location = new System.Drawing.Point(560, 241);
+            this.generar.Location = new System.Drawing.Point(279, 242);
             this.generar.Name = "generar";
             this.generar.Size = new System.Drawing.Size(76, 27);
-            this.generar.TabIndex = 0;
+            this.generar.TabIndex = 6;
             this.generar.Text = "Generar";
             this.generar.UseVisualStyleBackColor = true;
             this.generar.Click += new System.EventHandler(this.generar_Click);
@@ -55,18 +57,10 @@
             this.limpiar.Location = new System.Drawing.Point(13, 244);
             this.limpiar.Name = "limpiar";
             this.limpiar.Size = new System.Drawing.Size(75, 23);
-            this.limpiar.TabIndex = 1;
+            this.limpiar.TabIndex = 7;
             this.limpiar.Text = "Limpiar";
             this.limpiar.UseVisualStyleBackColor = true;
             this.limpiar.Click += new System.EventHandler(this.limpiar_Click);
-            // 
-            // rutas
-            // 
-            this.rutas.FormattingEnabled = true;
-            this.rutas.Location = new System.Drawing.Point(170, 121);
-            this.rutas.Name = "rutas";
-            this.rutas.Size = new System.Drawing.Size(312, 21);
-            this.rutas.TabIndex = 2;
             // 
             // fechaSalida
             // 
@@ -76,7 +70,7 @@
             this.fechaSalida.MinDate = new System.DateTime(2015, 11, 28, 0, 0, 0, 0);
             this.fechaSalida.Name = "fechaSalida";
             this.fechaSalida.Size = new System.Drawing.Size(150, 20);
-            this.fechaSalida.TabIndex = 3;
+            this.fechaSalida.TabIndex = 1;
             this.fechaSalida.Value = new System.DateTime(2015, 11, 28, 12, 16, 57, 0);
             // 
             // label1
@@ -105,48 +99,73 @@
             this.fechaLlegadaEstimada.MinDate = new System.DateTime(2015, 11, 28, 0, 0, 0, 0);
             this.fechaLlegadaEstimada.Name = "fechaLlegadaEstimada";
             this.fechaLlegadaEstimada.Size = new System.Drawing.Size(150, 20);
-            this.fechaLlegadaEstimada.TabIndex = 7;
+            this.fechaLlegadaEstimada.TabIndex = 2;
             this.fechaLlegadaEstimada.Value = new System.DateTime(2015, 11, 28, 12, 16, 57, 0);
+            // 
+            // destino
+            // 
+            this.destino.Location = new System.Drawing.Point(170, 195);
+            this.destino.Name = "destino";
+            this.destino.Size = new System.Drawing.Size(150, 20);
+            this.destino.TabIndex = 5;
+            // 
+            // origen
+            // 
+            this.origen.Location = new System.Drawing.Point(170, 158);
+            this.origen.Name = "origen";
+            this.origen.Size = new System.Drawing.Size(150, 20);
+            this.origen.TabIndex = 4;
+            // 
+            // matricula
+            // 
+            this.matricula.Location = new System.Drawing.Point(170, 121);
+            this.matricula.Name = "matricula";
+            this.matricula.Size = new System.Drawing.Size(150, 20);
+            this.matricula.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(39, 124);
+            this.label4.Location = new System.Drawing.Point(39, 198);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Ruta:";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Ciudad destino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Ciudad origen:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(39, 169);
+            this.label5.Location = new System.Drawing.Point(39, 124);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Aeronave:";
-            // 
-            // aeronaves
-            // 
-            this.aeronaves.FormattingEnabled = true;
-            this.aeronaves.Location = new System.Drawing.Point(170, 166);
-            this.aeronaves.Name = "aeronaves";
-            this.aeronaves.Size = new System.Drawing.Size(312, 21);
-            this.aeronaves.TabIndex = 10;
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Matricula aeronave:";
             // 
             // GenerarViajeForm
             // 
+            this.AcceptButton = this.generar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 288);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.aeronaves);
+            this.ClientSize = new System.Drawing.Size(375, 288);
+            this.Controls.Add(this.destino);
+            this.Controls.Add(this.origen);
+            this.Controls.Add(this.matricula);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.fechaLlegadaEstimada);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.fechaSalida);
-            this.Controls.Add(this.rutas);
             this.Controls.Add(this.limpiar);
             this.Controls.Add(this.generar);
             this.Name = "GenerarViajeForm";
@@ -161,13 +180,15 @@
 
         private System.Windows.Forms.Button generar;
         private System.Windows.Forms.Button limpiar;
-        private System.Windows.Forms.ComboBox rutas;
         private System.Windows.Forms.DateTimePicker fechaSalida;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker fechaLlegadaEstimada;
+        private System.Windows.Forms.TextBox destino;
+        private System.Windows.Forms.TextBox origen;
+        private System.Windows.Forms.TextBox matricula;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox aeronaves;
     }
 }
--- AerolineaFrba/AerolineaFrba/Generacion Viaje/GenerarViajeForm.cs
+++ AerolineaFrba/AerolineaFrba/Generacion Viaje/GenerarViajeForm.cs
@@ -20,50 +20,12 @@ namespace AerolineaFrba.Generacion_Viaje
         private Tipos_Servicio_RutasTableAdapter tipos_Servicio_RutasTableAdapter = new Tipos_Servicio_RutasTableAdapter();
         private VuelosTableAdapter vuelosTableAdapter = new VuelosTableAdapter();
         private GD2C2015DataSet dataSet = new GD2C2015DataSet();
-        private int[] rutasArray;
-        private int[] aeronavesArray;
 
         public GenerarViajeForm()
         {
             InitializeComponent();
             this.fechaSalida.Value = DateTime.Today;
             this.fechaLlegadaEstimada.Value = DateTime.Today;
-            this.fillRutas();
-            this.fillAeronaves();
-        }
-
-        private void fillAeronaves()
-        {
-            this.aeronaves.Items.Clear();
-            this.aeronavesTableAdapter.Fill(this.dataSet.Aeronaves);
-            this.tipos_ServicioTableAdapter.Fill(this.dataSet.Tipos_Servicio);
-            GD2C2015DataSet.AeronavesRow[] rows = (GD2C2015DataSet.AeronavesRow[])dataSet.Aeronaves.Select("aer_activo=1");
-            aeronavesArray = new int[rows.Count()];
-            int index = 0;
-            foreach (GD2C2015DataSet.AeronavesRow row in rows)
-            {
-                string aux = row.aer_matricula + " - " + row.aer_modelo + " - " + row.Tipos_ServicioRow.tip_descripcion;
-                this.aeronaves.Items.Insert(index, aux);
-                aeronavesArray[index] = row.aer_id;
-                index = index + 1;
-            }
-
-        }
-
-        private void fillRutas()
-        {
-            this.rutas.Items.Clear();
-            this.ciudadesTableAdapter.Fill(this.dataSet.Ciudades);
-            this.rutasTableAdapter.Fill(this.dataSet.Rutas);
-            GD2C2015DataSet.RutasRow[] rows = (GD2C2015DataSet.RutasRow[])dataSet.Rutas.Select("rut_activo=1");
-            rutasArray = new int[rows.Count()];
-            int index = 0;
-            foreach (GD2C2015DataSet.RutasRow row in rows){
-                string aux = row.CiudadesRowByFK__Rutas__ciudad_or__6B79F03D.ciu_descripcion + " - " + row.CiudadesRowByFK__Rutas__ciudad_de__6C6E1476.ciu_descripcion + " - " + row.rut_codigo.ToString();
-                this.rutas.Items.Insert(index, aux);
-                rutasArray[index] = row.rut_id;
-                index = index + 1;
-            }
         }
 
         private void GenerarViajeForm_Load(object sender, EventArgs e)
@@ -73,10 +35,27 @@ namespace AerolineaFrba.Generacion_Viaje
 
         private void generar_Click(object sender, EventArgs e)
         {
-            if (this.valido())
+            this.aeronavesTableAdapter.Fill(this.dataSet.Aeronaves);
+            this.rutasTableAdapter.Fill(this.dataSet.Rutas);
+
+            GD2C2015DataSet.AeronavesRow[] aeronave = (GD2C2015DataSet.AeronavesRow[])this.dataSet.Aeronaves.Select("aer_matricula='" + matricula.Text + "' AND aer_activo=1");
+            if (aeronave.Length == 0)
             {
-                int aeronaveId = this.aeronavesArray[this.aeronaves.SelectedIndex];
-                int rutaId = this.rutasArray[this.rutas.SelectedIndex];
+                MessageBox.Show("No existe el aeronave con esa matrícula\n");
+                return;
+            }
+            int aeronaveId = aeronave.First().aer_id;
+
+            GD2C2015DataSet.CiudadesDataTable ciudadOrigen = this.ciudadesTableAdapter.GetDataByDescripcionLower(origen.Text);
+
+            GD2C2015DataSet.CiudadesDataTable ciudadDestino = this.ciudadesTableAdapter.GetDataByDescripcionLower(destino.Text);
+
+            if (this.valido(aeronaveId, ciudadOrigen, ciudadDestino))
+            {
+                this.origen.Text = ciudadOrigen.First().ciu_descripcion;
+                this.destino.Text = ciudadDestino.First().ciu_descripcion;
+                
+                int rutaId = this.getRutaId(ciudadOrigen, ciudadDestino);
 
                 try
                 {
@@ -98,17 +77,31 @@ namespace AerolineaFrba.Generacion_Viaje
             }
         }
 
-        private bool valido()
+        private bool valido(int aeronaveId, GD2C2015DataSet.CiudadesDataTable origen, GD2C2015DataSet.CiudadesDataTable destino)
         {
             string error = null;
 
-            if (this.rutas.Text == "" || this.rutas.SelectedIndex == -1)
+            if (this.matricula.Text == "")
             {
-                error = "La ruta no puede ser nula\n";
+                error = "La matricula no puede ser nula\n";
             }
-            if (this.aeronaves.Text == "" || this.aeronaves.SelectedIndex == -1)
+
+            if (this.origen.Text == "")
             {
-                error = error + "Debe seleccionar un aeronave\n";
+                error = error + "Debe seleccionar una ciudad origen\n";
+            }
+            else if (origen.Count == 0)
+            {
+                error = error + "No existe la ciudad de origen\n";
+            }
+
+            if (this.destino.Text == "")
+            {
+                error = error + "Debe seleccionar una ciudad destino\n";
+            }
+            else if (destino.Count == 0)
+            {
+                error = error + "No existe la ciudad de destino\n";
             }
             if ((this.fechaSalida.Value.Date.CompareTo(DateTime.Now.Date) < 0) || (this.fechaSalida.Value.Date.CompareTo(DateTime.Now.Date) == 0 && this.fechaSalida.Value.TimeOfDay.CompareTo(DateTime.Now.TimeOfDay) < 0))
             {
@@ -126,11 +119,13 @@ namespace AerolineaFrba.Generacion_Viaje
             }
             else
             {
-                if (this.serviciosNoConcuerdan())
+                int rutaId = this.getRutaId(origen, destino);
+
+                if (this.serviciosNoConcuerdan(aeronaveId, rutaId))
                 {
                     error = error + "El servicio del aeronave no concuerda con el servicio de la ruta\n";
                 }
-                if (this.aeronaveNoDisponible())
+                if (this.aeronaveNoDisponible(aeronaveId))
                 {
                     error = error + "El aeronave seleccionada no se encuentra disponible para la fecha ingresada\n";
                 }
@@ -143,10 +138,22 @@ namespace AerolineaFrba.Generacion_Viaje
             return true;
         }
 
-        private bool aeronaveNoDisponible()
+        private int getRutaId(GD2C2015DataSet.CiudadesDataTable origen, GD2C2015DataSet.CiudadesDataTable destino)
+        {
+            int rutaId = 0;
+            int ciudad_origen_id = origen.First().ciu_id;
+            int ciudad_destino_id = destino.First().ciu_id;
+            GD2C2015DataSet.RutasDataTable ruta = this.rutasTableAdapter.GetDataByOrigenDestino(ciudad_origen_id, ciudad_destino_id);
+            if (ruta.Count() > 0)
+            {
+                rutaId = ruta.First().rut_id;
+            }
+            return rutaId;
+        }
+
+        private bool aeronaveNoDisponible(int aeronaveId)
         {
             bool result = false;
-            int aeronaveId = this.aeronavesArray[this.aeronaves.SelectedIndex];
             DateTime fecha = this.fechaSalida.Value;
             this.vuelosTableAdapter.Fill(this.dataSet.Vuelos);
             GD2C2015DataSet.VuelosRow[] row = (GD2C2015DataSet.VuelosRow[])this.dataSet.Vuelos.Select("aeronave_id='" + aeronaveId + "' AND vue_fecha_salida='" + fecha + "'");
@@ -157,11 +164,9 @@ namespace AerolineaFrba.Generacion_Viaje
             return result;
         }
 
-        private bool serviciosNoConcuerdan()
+        private bool serviciosNoConcuerdan(int aeronaveId,int rutaId)
         {
             bool result = false;
-            int aeronaveId = this.aeronavesArray[this.aeronaves.SelectedIndex];
-            int rutaId = this.rutasArray[this.rutas.SelectedIndex];
             int tipoServicioId = 0;
             this.aeronavesTableAdapter.Fill(this.dataSet.Aeronaves);
             GD2C2015DataSet.AeronavesRow row1 = (GD2C2015DataSet.AeronavesRow)this.dataSet.Aeronaves.Select("aer_id='" + aeronaveId + "'").First();
@@ -177,8 +182,9 @@ namespace AerolineaFrba.Generacion_Viaje
 
         private void limpiar_Click(object sender, EventArgs e)
         {
-            this.rutas.ResetText();
-            this.aeronaves.ResetText();
+            this.matricula.ResetText();
+            this.origen.ResetText();
+            this.destino.ResetText();
             this.fechaSalida.Value = DateTime.Today;
             this.fechaLlegadaEstimada.Value = DateTime.Today;
         }
